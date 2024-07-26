37a7ba7eadbd97f83f6ee90afee855663bf09b7f
Reinaldo Leal de Azevedo Filho <rlaf@cesar.org.br>
2024-07-26 08:10:47 -0300

    iniciando o projeto de design system

---
+++ src/componentes/AbBotao/AbBotao.styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const BotaoEstilizado = styled.button`
+  background: #eb9b00;
+  padding: 16px 32px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  border: 2px solid #eb9b00;
+  cursor: pointer;
+
+  &:hover {
+    background: #b87900;
+    border: 2px solid #b87900;
+  }
+`;
---
+++ src/componentes/AbBotao/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {BotaoEstilizado} from './AbBotao.styled';
+
+export const AbBotao = () => {
+    return (<BotaoEstilizado>Clique aqui!</BotaoEstilizado>)
+}
